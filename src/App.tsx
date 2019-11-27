56b064efe19621816f1f618e626aa9ccbd53a0e7
Evan Thomas <evan@tempoautomation.com>
2019-11-26 18:14:45 -0800

    Data displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+type PersonName = string;
+type Ranking = {
+  [book: string]: number;
+};
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const ranking1 = {'book 1': 1, 'book 2': 2, 'book 3': 3};
+  const ranking2 = {'book 1': 3, 'book 2': 2, 'book 3': 1};
+  const ranking3 = {'book 1': 2, 'book 2': 1, 'book 3': 45};
+  const peopleWithRankings: [PersonName, Ranking][] = [
+    ['person 1', ranking1],
+    ['person 2', ranking2],
+    ['person 3', ranking3],
+  ];
+
+  // TODO: make sure al people have all the same book keys
+  const books = Object.keys(ranking1);
+
+  const tableData = books.map(book => (
+    <tr>
+      <td>{book}</td>
+      {peopleWithRankings.map(([, ranking]) => (
+        <td>{ranking[book]}</td>
+      ))}
+    </tr>
+  ));
+
+  const tableHeader = (
+    <tr>
+      <th></th> {/* empty header for the books row */}
+      {peopleWithRankings.map(([person]) => (
+        <th>{person}</th>
+      ))}
+    </tr>
   );
-}
+
+  return (
+    <table>
+      {tableHeader}
+      {tableData}
+    </table>
+  );
+};
 
 export default App;
