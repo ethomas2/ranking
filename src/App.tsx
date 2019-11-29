f43c9ec9761c239f1a8a658344a9f9fe1abd8c18
Evan Thomas <evan@tempoautomation.com>
2019-11-29 09:57:05 -0800

    Css it

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const defaultTableData = [
-  ['a', 'b', 'c'],
-  ['e', 'f', 'g'],
-  ['h', 'i', 'j'],
-  ['h', 'i', 'j'],
+  ['1', '3', '1', '2', '1', '3'],
+  ['3', '1', '2', '1', '2', '1'],
+  ['2', '2', '3', '3', '3', '2'],
 ];
-const defaultTableLeftCol = [
+const defaultTableHeader = [
   'Evan',
-  'Adrian',
-  'G',
-  'Micheala',
+  'Michaela',
   'Dave',
   'Linlin',
+  'Adrian',
+  'G',
+];
+const defaultTableLeftCol = [
+  'Book 1 -- The end of the beggining was the beggining of the end',
+  'Book 2',
+  'Book 3',
 ];
 const App: React.FC = () => {
   // TODO: useArrState might be useful here?
   const [tableBodyData, setTableData] = useState<string[][]>(defaultTableData);
-  const [tableHeaderData, setTableHeader] = useState<string[]>([
-    'h1',
-    'h2',
-    'h3',
-  ]);
+  const [tableHeaderData, setTableHeader] = useState<string[]>(
+    defaultTableHeader,
+  );
   const [tableLeftColData, setTableLeftCol] = useState<string[]>(
     defaultTableLeftCol,
   );
 
-  // TODO: maybe just use css grid instead of this table stuff. table stuff is
-  // verbose
   const tableHeaderContent = (
     <tr>
       <th />
       {tableHeaderData.map((item, i) => (
         <th key={`header-${i}`}>
           <input
+            type="text"
+            size={4}
             value={item}
             onChange={e =>
               setTableHeader(setArr(tableHeaderData, i, e.target.value))
@@ -49,7 +50,7 @@ const App: React.FC = () => {
   const tableBodyContent = tableBodyData.map((row, rowIdx) => (
     <tr key={`row-${rowIdx}`}>
       <td>
-        <input
+        <textarea
           value={tableLeftColData[rowIdx]}
           onChange={e =>
             setTableLeftCol(setArr(tableLeftColData, rowIdx, e.target.value))
@@ -57,8 +58,11 @@ const App: React.FC = () => {
         />
       </td>
       {row.map((item, colIdx) => (
-        <td key={`cell-${rowIdx}-${colIdx}`}>
+        <td key={`cell-${rowIdx}-${colIdx}`} className="Table__td-cell">
           <input
+            size={1}
+            type="text"
+            className="Table__input-cell"
             value={item}
             onChange={e =>
               setTableData(
@@ -72,10 +76,19 @@ const App: React.FC = () => {
   ));
 
   return (
-    <table>
-      <thead>{tableHeaderContent}</thead>
-      <tbody>{tableBodyContent}</tbody>
-    </table>
+    <div className="App">
+      <div className="App__title-container">
+        <h2 className="App__title">Votes on Votes on Votes</h2>
+      </div>
+      <table>
+        <thead>{tableHeaderContent}</thead>
+        <tbody>{tableBodyContent}</tbody>
+      </table>
+      <div>
+        <input value="Add Person" type="button" />
+        <input value="Add Book" type="button" />
+      </div>
+    </div>
   );
 };
 
