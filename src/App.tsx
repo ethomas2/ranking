10606b3a0f0a42f83727f5e3f86358ab16e9da4d
Evan Thomas <evan@tempoautomation.com>
2019-11-29 00:07:04 -0800

    Make inputs controlled inputs

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const defaultTableLeftCol = [
   'Linlin',
 ];
 const App: React.FC = () => {
+  // TODO: useArrState might be useful here?
   const [tableBodyData, setTableData] = useState<string[][]>(defaultTableData);
   const [tableHeaderData, setTableHeader] = useState<string[]>([
     'h1',
@@ -33,19 +34,37 @@ const App: React.FC = () => {
     <tr>
       {tableHeaderData.map((item, i) => (
         <th key={`header-${i}`}>
-          <input value={item} />
+          <input
+            value={item}
+            onChange={e =>
+              setTableHeader(setArr(tableHeaderData, i, e.target.value))
+            }
+          />
         </th>
       ))}
     </tr>
   );
-  const tableBodyContent = tableBodyData.map((row, rowIndex) => (
-    <tr key={`row-${rowIndex}`}>
+
+  const tableBodyContent = tableBodyData.map((row, rowIdx) => (
+    <tr key={`row-${rowIdx}`}>
       <td>
-        <input value={tableLeftColData[rowIndex]} />
+        <input
+          value={tableLeftColData[rowIdx]}
+          onChange={e =>
+            setTableLeftCol(setArr(tableLeftColData, rowIdx, e.target.value))
+          }
+        />
       </td>
-      {row.map((item, colIndex) => (
-        <td key={`cell-${rowIndex}-${colIndex}`}>
-          <input value={item} />
+      {row.map((item, colIdx) => (
+        <td key={`cell-${rowIdx}-${colIdx}`}>
+          <input
+            value={item}
+            onChange={e =>
+              setTableData(
+                setArr2d(tableBodyData, rowIdx, colIdx, e.target.value),
+              )
+            }
+          />
         </td>
       ))}
     </tr>
@@ -60,3 +79,20 @@ const App: React.FC = () => {
 };
 
 export default App;
+
+function setArr<T>(arr: T[], idx: number, val: T): T[] {
+  const copy = [...arr];
+  copy[idx] = val;
+  return copy;
+}
+
+function setArr2d<T>(
+  arr: T[][],
+  rowIdx: number,
+  colIdx: number,
+  val: T,
+): T[][] {
+  const copy = arr.map(row => [...row]);
+  copy[rowIdx][colIdx] = val;
+  return copy;
+}
