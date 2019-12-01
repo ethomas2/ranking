49c40a80f568f1bdca1f2b8e4a70dfc312f6711d
Evan Thomas <evan@tempoautomation.com>
2019-11-30 20:47:46 -0800

    Add create new button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import MainPage from './MainPage';
 import HomePage from './HomePage';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 const App: React.FC = () => {
-  const mainPage = <MainPage />;
   return (
     <div className="App">
       <div className="App__title-container">
         <h2 className="App__title">Votes on Votes on Votes</h2>
       </div>
       <Router>
-        <Route path="/" component={HomePage} />
+        <Route path="/" exact component={HomePage} />
         <Route path="/election/:id" component={MainPage} />
       </Router>
     </div>
--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
+
+function req<T>(url: string, options: RequestInit): Promise<T> {
+  return fetch(url, options).then(resp => (resp.json() as unknown) as T);
+}
 
 const HomePage: React.FC = () => {
-  return <input type="button" value="Create New" />;
+  const history = useHistory();
+  const createNew = async () => {
+    const result = await req<{id: number}>('http://localhost:8000/election', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+    history.push(`/election/${result.id}`);
+  };
+  return <input type="button" value="Create New" onClick={createNew} />;
 };
 export default HomePage;
