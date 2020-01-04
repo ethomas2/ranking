af60981cfb634a8b91311602b50b2d7f7894f501
Evan Thomas <evan@tempoautomation.com>
2020-01-04 08:59:46 -0800

    Refactor a bit

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,12 @@
 #[macro_use]
 extern crate rocket;
 
-// use std::boxed;
 use std::env;
 use std::error;
 use std::fs;
 use std::io;
 
-type HeapError = Box<dyn error::Error>;
+type Result<T> = std::result::Result<T, Box<dyn error::Error>>;
 
 #[get("/")]
 fn get() -> &'static str {
@@ -17,35 +16,37 @@ fn get() -> &'static str {
 }
 
 #[post("/elections")]
-fn post() -> Result<String, HeapError> {
-    let db_dir = get_db_dir()?;
+fn post() -> Result<String> {
+    Ok(String::from("success"))
+}
+
+fn create_db_dir() -> Result<String> {
+    // TODO: add a load_config fn that you call as first line of main() that loads this into a
+    // global config variable
+    let DEFAULT_DB_DIR = String::from("db");
+
+    let db_dir_result: Result<String> = match env::var("APP_DB_DIR") {
+        Ok(x) => Ok(x),
+        Err(env::VarError::NotPresent) => Ok(DEFAULT_DB_DIR),
+        Err(x) => Err(Box::new(x)),
+    };
+    let db_dir = db_dir_result?;
 
     // create dir and change ErrorKind to pass
-    if let Err(e) = fs::create_dir(db_dir) {
+    if let Err(e) = fs::create_dir(&db_dir) {
         if e.kind() != io::ErrorKind::AlreadyExists {
             Err(e)?;
         }
     }
 
-    Ok(String::from("success"))
-}
-
-fn get_db_dir() -> Result<String, HeapError> {
-    let default = String::from("db");
-
-    let x = match env::var("APP_DB_DIR") {
-        Ok(x) => Ok(x),
-        Err(x) => {
-            if x == env::VarError::NotPresent {
-                return Ok(default);
-            } else {
-                return Err(Box::new(x));
-            }
-        }
-    };
-    return x;
+    Ok(db_dir)
 }
 
 fn main() {
+    if let Err(e) = create_db_dir() {
+        println!("{}", e);
+        std::process::exit(1);
+    }
+
     rocket::ignite().mount("/", routes![get, post]).launch();
 }
